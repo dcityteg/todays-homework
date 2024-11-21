f414ac02f1c151c33cc91599bf383e77b17361b4
FunFy <132677224+dcityteg@users.noreply.github.com>
2024-11-21 19:16:22 +0800

    update

--- routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getSetHomeworkPage, updatePassword } = require('../controllers/auth');
-
-const router = express.Router();
-
-router.get('/setc', getSetHomeworkPage);
-router.post('/set-password', updatePassword);
-
-module.exports = router;
