b62b8853ada4dd09a20b2291a2d47b6df666a8e8
FunFy <132677224+dcityteg@users.noreply.github.com>
2024-12-06 21:26:56 +0800

    zeabur

---
+++ src/api/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const multer = require('multer');
+const { Pool } = require('pg');
+const { JSDOM } = require('jsdom');
+const DOMPurify = require('dompurify');
+const { marked } = require('marked');
+
+// App and Middleware setup
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Database and file upload setup
+const pool = require('./r/db');
+const upload = require('./r/multer');
+const DOMPurifyInstance = DOMPurify(new JSDOM('').window);
+
+// Routes
+const setcRoute = require('./r/setc');
+const homeworkRoute = require('./r/homework');
+const setPasswordRoute = require('./r/set-password');
+const userRoute = require('./r/user');  // 导入 user 路由
+
+app.use('/', homeworkRoute);
+app.use('/setc', setcRoute);
+app.use('/setc/user', userRoute);  // 更新为 /setc/user 路由
+app.use('/set-password', setPasswordRoute);
+
+// Database table setup
+const { checkAndCreateTables } = require('./r/db');
+checkAndCreateTables();
+
+module.exports = app;
